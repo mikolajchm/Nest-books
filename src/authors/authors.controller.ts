a665b95dd55cac7ee04f8b82b94e0a5816a61058
mikolajchm <chmielewski.5454@gmail.com>
2024-09-26 10:10:19 +0200

    First commit

---
+++ src/authors/authors.controller.ts
@@ -0,0 +1,50 @@
+import { Controller, Get, Param, Delete, Body, Post, Put } from '@nestjs/common';
+import { AuthorsService } from './authors.service';
+import { CreateAuthorDTO } from './dtos/create-author.dto';
+import { UpdateAuthorDTO } from './dtos/update-author.dto';
+import { ParseUUIDPipe } from '@nestjs/common';
+import { NotFoundException } from '@nestjs/common';
+
+@Controller('authors')
+export class AuthorsController {
+  constructor(private authorsService: AuthorsService) {}
+
+    @Get('/')
+      getAll(): any {
+        return this.authorsService.getAll();
+    }
+
+    @Get('/:id')
+      async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+      const author = await this.authorsService.getById(id);
+      if (!author) throw new NotFoundException('Author not found');
+      return author;
+    }
+
+    
+    @Post('/')
+    create(@Body() authorData: CreateAuthorDTO) {
+        return this.authorsService.create(authorData);
+    } 
+    
+    @Put('/:id')
+    async update(
+      @Param('id', new ParseUUIDPipe()) id: string,
+      @Body() authorData: UpdateAuthorDTO,
+    ) {
+      if (!(await this.authorsService.getById(id)))
+        throw new NotFoundException('Author not found');
+
+      await this.authorsService.updateById(id, authorData);
+      return { success: true };
+    }
+
+    @Delete('/:id')
+    async deleteById(@Param('id', new ParseUUIDPipe()) id: string) {
+      if (!(await this.authorsService.getById(id)))
+        throw new NotFoundException('Author not found');
+      await this.authorsService.deleteById(id);
+      return { success: true };
+    }
+
+}
